1f25311e785a4e6b061dc28ef0aa6ec905c5d4fe
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-08-04 00:20:29 +0800

    Update page.tsx

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -1,7 +1,7 @@
 // src/app/user/[username]/page.tsx
 
 import Profile from '@/components/Profile'
-import { primsa } from '@prisma/client'
+import { Primsa } from '@prisma/client'
 
 export default async function UserProfilePage({ params }: { params: { username: string } }) {
   const user = await prisma.user.findUnique({
